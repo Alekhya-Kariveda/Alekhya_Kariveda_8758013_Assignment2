cb02489bea9068387e396fa0499a98be6d4573db
Alekhya Kariveda <akariveda8013@conestogac.on.ca>
2021-10-06 02:42:02 -0400

    Initial git commit

---
+++ Alekhya_Kariveda_8758013_Assignment2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Alekhya_Kariveda_8758013_Assignment2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Alekhya_Kariveda_8758013_Assignment2")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f60c2a22-c207-4b53-83bc-fe42812cb8dc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Alekhya_Kariveda_8758013_Assignment2/RectangleTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Alekhya_Kariveda_8758013_Assignment2
+{
+    [TestClass]
+    public class RectangleTest
+    {
+        Rectangle r;
+        [TestMethod]
+        public void TestGetLength()
+        {
+            r = new Rectangle(20, 30);
+            Assert.AreEqual(20, r.GetLength());
+        }
+        [TestMethod]
+        public void TestGetWidth()
+        {
+            r = new Rectangle(20, 30);
+            Assert.AreEqual(30, r.GetWidth());
+        }
+        [TestMethod]
+        public void TestGetPerimeter()
+        {
+            r = new Rectangle(20, 30);
+            Assert.AreEqual(100, r.GetPerimeter());
+        }
+        [TestMethod]
+        public void TestGetArea()
+        {
+            r = new Rectangle(20, 30);
+            Assert.AreEqual(600, r.GetArea());
+        }
+
+    }
+}
